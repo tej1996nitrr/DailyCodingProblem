87573049cb218556d8390e0f61834d9cada1e217
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-12 18:29:56 +0530

    sol day12

--- Day12.py
+++ Day12.py
@@ -21,4 +21,21 @@ res = []
 a,b = 1,2
 N = 4
 max_ = N
-
+combos = list()
+    
+def get_step_combos(num_steps, step_sizes):
+    combos = list()
+    
+    if num_steps < min(step_sizes):
+        return combos
+    
+    for step_size in step_sizes:
+        if num_steps == step_size:
+            combos.append([step_size])
+        elif num_steps > step_size:
+            child_combos = get_step_combos(num_steps - step_size, step_sizes)
+            for child_combo in child_combos:
+                combos.append([step_size] + child_combo)
+    return combos
+    
+assert get_step_combos(4, [1, 2]) == [[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2]]
